cb3e7c45af11cebda185ad21151f2abe48d49705
Kuznetsov Eric <kuznetsov.eric@gmail.com>
2021-05-17 18:12:22 +0300

    Basic setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import { App } from "./app";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  const { getByRole } = render(<App />);
+  const main = getByRole("main");
+  expect(main).toBeInTheDocument();
 });
